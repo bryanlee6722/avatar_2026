6f195a2ec6e3fa021271ac9049df601ecb23c213
storm5030 <storm5030@gmail.com>
2026-01-30 13:48:27 +0900

    Edit rviz

--- avatar_bringup/launch/rviz_follower.launch.py
+++ avatar_bringup/launch/rviz_follower.launch.py
@@ -5,37 +5,45 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    pkg_share = FindPackageShare('avatar_description')
-    xacro_file = PathJoinSubstitution([pkg_share, 'urdf/follower', 'follower.urdf.xacro'])
-    rviz_config = PathJoinSubstitution([pkg_share, 'rviz', 'avatar_model.rviz'])
+    pkg_share = FindPackageShare('avatar_bringup')
+    pkg_description = FindPackageShare('avatar_description')
+
+    xacro_file = PathJoinSubstitution([pkg_description, 'urdf/follower', 'follower.urdf.xacro'])
+    rviz_config = PathJoinSubstitution([pkg_description, 'rviz', 'avatar_model.rviz'])
+    controllers_file = PathJoinSubstitution([pkg_share, 'config', 'follower', 'hardware_controller_manager.yaml'])
 
     robot_description_content = ParameterValue(
-        Command(['xacro ', xacro_file]),
-        value_type=str
+    Command([
+        'xacro ',
+        xacro_file,
+        ' use_sim:=false',
+        ' use_mock_hardware:=true',
+        ' mock_sensor_commands:=false'
+    ]),
+    value_type=str
+)
+
+
+    control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_description_content}, controllers_file],
+        output='screen'
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+    )
+
+    arm_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['arm_controller'],
     )
 
     return LaunchDescription([
-        # Joint State Publisher GUI 추가
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui',
-            output='screen'
-        ),
-
-        # Trajectory -> JointState bridge
-        Node(
-            package='avatar_bringup',
-            executable='traj_to_jointstate',
-            name='traj_to_jointstate',
-            output='screen',
-            parameters=[
-                {'traj_topic': '/arm_controller/joint_trajectory'},   # /leader/joint_trajectory로 변경
-                {'joint_states_topic': '/joint_states'}
-            ]
-        ),
-
-        # robot_state_publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -44,7 +52,10 @@ def generate_launch_description():
             parameters=[{'robot_description': robot_description_content}]
         ),
 
-        # rviz2 실행
+        control_node,
+        joint_state_broadcaster_spawner,
+        arm_controller_spawner,
+
         Node(
             package='rviz2',
             executable='rviz2',
@@ -53,4 +64,3 @@ def generate_launch_description():
             output='screen'
         )
     ])
-1
